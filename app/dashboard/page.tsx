986122f8b504e95ee5e82699d181ada8da90663a
ahmniab <ahmniab11@gmail.com>
2026-02-03 21:44:34 +0200

    feat: add DashboardLayout and DashboardPage components

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+
+import Dashboard from '@/components/dashboard';
+
+
+const DashboardPage = () => {
+
+  return (
+    <Dashboard 
+      plantsCount={69}
+      diseasesCount={69}
+    />
+  );
+};
+
+export default DashboardPage;
