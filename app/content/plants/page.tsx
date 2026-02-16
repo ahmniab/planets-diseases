d6fff1d0bd158b9fd960b9de42053b943bd9a086
ahmniab <ahmniab11@gmail.com>
2026-02-16 19:08:26 +0200

    feat: enhance Plants component with search and pagination functionality

--- app/content/plants/page.tsx
+++ app/content/plants/page.tsx
@@ -1,18 +1,26 @@
 
 import { FC } from "react";
 import { 
+    Container,
     Box,
-    Container
- } from "@mui/material";
+} from "@mui/material";
 import PlantsGrid from "@/components/PlantsGrid";
-import { getAllPlants } from "@/lib/firebaseAdmin/database";
+import { getAllPlants, getAllPlantsCount } from "@/lib/firebaseAdmin/database";
 import Link from "next/link";
+import SearchBar from "@/components/shared/SearchBar";
+import CustomPagination from "@/components/shared/Pagination";
+import NoResults from "@/components/NoResults";
 
-const Plants: FC = async () => {
+
+async function Plants({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) { 
+    const query = (await searchParams).q as string || "";
+    const page = parseInt((await searchParams).p as string) || 1;
+    
     let plants = [];
+    let pageCount = 0; 
     try {
-        plants = await getAllPlants();
-
+        plants = await getAllPlants(query, page);
+        pageCount = Math.ceil(await getAllPlantsCount(query) / 10);
     } catch (error) {
         return (
             <Link href="/not-found" />
@@ -20,9 +28,30 @@ const Plants: FC = async () => {
     }
 
     return (
-        <Container sx={{ p: 2 }}>
-            <PlantsGrid plants={plants ?? []} />
-        </Container>
+        <>
+            <Container sx={{ p: 2, minHeight: "75vh" }}>
+                <SearchBar 
+                    initialSearchText={query} 
+                    navigateString={`/content/plants?q=%s&p=1`}
+                />
+                { 
+                (query !== '' && plants.length === 0) ? 
+                    <NoResults message={`لم يتم العثور على نباتات تطابق بحثك: "${query}"`} /> :
+                    <PlantsGrid plants={plants ?? []} />
+                }
+            </Container>
+            <Box sx={{ 
+                display: "flex", 
+                justifyContent: "center", 
+                mt: 0,
+            }} >
+                {plants.length > 0 && <CustomPagination 
+                    count={pageCount} 
+                    page={page}
+                    navigateString={`/content/plants?q=${query}&p=%s`}
+                />}
+            </Box>
+        </>
     );
 }
 export default Plants;
