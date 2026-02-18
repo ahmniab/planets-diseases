16fcf93c9cb747231c11d35e9a0fa3ccb3016406
ahmniab <ahmniab11@gmail.com>
2026-02-18 22:34:53 +0200

    feat: simplify AuthGuard by removing unused state and redirect logic

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -1,28 +1,14 @@
 'use client';
-import { auth } from '@/lib/firebase/config';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useAuth } from '@/contexts/AuthContext';
+import { redirect } from 'next/navigation';
 
 const AuthGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [user, setUser] = useState<any>(null);
-    const router = useRouter();
+    const { isAuthenticated } = useAuth();
+    if (isAuthenticated) {
+      return <>{children}</>;
+    }
 
-    useEffect(() => {
-    // Subscribe to auth state changes
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-      
-      // Optional: If you want to force redirect immediately upon logout detection globally
-      if (!user) {
-        router.push('/login'); 
-      }
-    });
-
-    return () => unsubscribe();
-  }, [router]);
-
-
-    return <>{children}</>;
+    return redirect('/login');
 };
 
 export default AuthGuard;
